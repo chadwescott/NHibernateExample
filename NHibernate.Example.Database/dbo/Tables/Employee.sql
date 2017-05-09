c22fd786d632dfa30278b678f4c89e3aed1862bc
Chad Wescott <cwescott@coopervision.com>
2017-05-09 10:21:58 -0400

    Added database project, employee table, and comparison file.

---
+++ NHibernate.Example.Database/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (50)  NULL,
+    [LastName]    VARCHAR (50)  NULL,
+    [Designation] VARCHAR (100) NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
